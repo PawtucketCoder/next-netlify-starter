b2f0b78c53a5c42c1d8497c866743f85f3056b10
Michael S <125391279+PawtucketCoder@users.noreply.github.com>
2023-07-23 03:12:00 -0400

    sdfsdf

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,8 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { useEffect } from 'react'; // Import useEffect from React
 
 export default function Home() {
-  useEffect(() => {
-    // Move the JavaScript code inside useEffect hook
-    function animateEllipsis() {
-      const ellipsisElement = document.getElementById('animatedEllipsis');
-      let count = 0;
-      const interval = setInterval(() => {
-        count++;
-        if (count > 3) count = 0;
-
-        ellipsisElement.textContent = 'Loading' + '.'.repeat(count);
-      }, 500); // Change the interval to control the speed of the animation
-
-      return () => clearInterval(interval); // Cleanup the interval when the component unmounts
-    }
-
-    animateEllipsis();
-  }, []); // Empty dependency array means this effect runs only once, similar to componentDidMount
-
   return (
     <div className="container">
       <Head>
@@ -30,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 id="animatedEllipsis">Loading</h1> {/* Set initial text content to "Loading" */}
+        <Header title="..." />
       </main>
 
       <Footer />
